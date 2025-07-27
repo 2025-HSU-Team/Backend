fa9b402a7a031d673f62eb111ed3057a27a7bed6
gimn70009 <risa1110@hansung.ac.kr>
2025-07-27 17:30:36 +0900

    chore: 프로젝트 초기 설정 및 application.properties gitignore 등록

---
+++ src/main/java/com/sosaw/sosaw/SosawApplication.java
@@ -0,0 +1,13 @@
+package com.sosaw.sosaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sosaw/sosaw/SosawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosaw.sosaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
