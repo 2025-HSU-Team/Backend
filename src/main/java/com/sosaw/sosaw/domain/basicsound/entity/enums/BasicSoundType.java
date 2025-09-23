cd6bbe8c7c0cc395a01e85a68f3b01e397f4e99a
LgE02 <leegaeun023@gmail.com>
2025-09-23 20:58:37 +0900

    chore : 기본 소리 라벨 9개로 수정

--- src/main/java/com/sosaw/sosaw/domain/basicsound/entity/enums/BasicSoundType.java
+++ src/main/java/com/sosaw/sosaw/domain/basicsound/entity/enums/BasicSoundType.java
@@ -10,17 +10,18 @@ import java.util.Arrays;
 public enum BasicSoundType {
     DOG_BARK("Dog Bark"),        // 강아지 짓는 소리
     CAT_MEOW("Cat Meow"),        // 고양이 소리
-    HUMAN_LAUGH("Laughing"),     // 사람 웃는 소리
     BABY_CRY("Baby Cry"),        // 아기 우는 소리
     PHONE_RING("Phone Ring"),      // 전화벨 소리
     DOORBELL("Doorbell"),        // 초인종 소리
     DOOR_OPEN_CLOSE("Door In-Use"), // 문 여닫는 소리
-    KNOCK("Knocking"),           // 노크 소리
     FIRE_ALARM("Fire/Smoke Alarm"),      // 화재 경보기 소리
-    MICROWAVE("Microwave"),       // 전자레인지 소리
     CAR_HORN("Car Honk"),       // 경적 소리
     SIREN("Siren"),          // 비상 경보음 소리
     UNKNOWN("Unknown");
+
+//    HUMAN_LAUGH("Laughing"),     // 사람 웃는 소리
+//    KNOCK("Knocking"),           // 노크 소리
+//    MICROWAVE("Microwave"),       // 전자레인지 소리
 
     private final String label;
 
